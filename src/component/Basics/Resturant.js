4582f49ca242799832d1fd173dafa8f26f91fb49
jaydeep jain <jainjaydeep.jj@gmail.com>
2023-03-25 01:09:46 +0530

    initialWorkingDone

---
+++ src/component/Basics/Resturant.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./style.css"
+const Resturant = () => {
+  return (
+    <>
+    <div className='card-container'>
+      <div className='card'> 
+      <div className='card-body'>
+        <span className='card-number card-circle subtle'> 1</span>
+        <span className="card-author subtle"> BreakFast</span>
+        <h2 className='card-description subtle'></h2>
+      </div>
+      
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Resturant
