5e7d36f72f69df4753eb850d085a3eb174bc94af
jaydeep jain <jainjaydeep.jj@gmail.com>
2023-03-25 03:14:27 +0530

    projectCompleted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react";
+import Resturant from "./component/Basics/Resturant";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
 
-import Resturant from './component/Basics/Resturant'
 const App = () => {
-  return <Resturant/>
-}
+  return <Resturant />;
+};
 
-export default App
+export default App;
--- src/component/Basics/Resturant.js
+++ src/component/Basics/Resturant.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import "./style.css"
+import React, { useState } from "react";
+import "./style.css";
+import Menu from "./menuApi.js";
+import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+console.log(uniqueList);
+
 const Resturant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
   return (
     <>
-    <div className='card-container'>
-      <div className='card'> 
-      <div className='card-body'>
-        <span className='card-number card-circle subtle'> 1</span>
-        <span className="card-author subtle"> BreakFast</span>
-        <h2 className='card-description subtle'></h2>
-      </div>
-      
-      </div>
-
-    </div>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
     </>
-  )
-}
+  );
+};
 
-export default Resturant
+export default Resturant;
